25da00a0a30df154277ee3fe2df7853d78bf05a6
nagipan777 <nagipan777@gmail.com>
2019-03-27 13:03:27 +0900

    full change

--- bot.py
+++ bot.py
@@ -1,150 +1,89 @@
 import os
+from flask import Flask, request
 import requests
-import json
 import redis
-import tensorflow as tf
-import multiprocessing as mp
-
-
-from flask import Flask
-from flask import request
-
-MODE = 'docomo'
-REDIS_URL = os.environ.get('REDIS_URL') # herokuによって登録済み
-LINE_API_PROFILE = 'https://api.line.me/v2/bot/profile'
-LINE_API_REPLY ='https://api.line.me/v2/bot/message/reply'
-LINE_HEADERS = {
-    'Content-type': 'application/json',
-    'Authorization': 'Bearer {}'.format(os.environ.get('CHANNEL_ACCESS_TOKEN'))
-}
-
-DOCOMO_API_KEY = os.environ.get('DOCOMO_API_KEY')
-DOCOMO_API_DIALOGUE = 'https://api.apigw.smt.docomo.ne.jp/naturalChatting/v1/dialogue'
-DOCOMO_HEADERS = {
-    'Content-type': 'application/json'
-}
-# 20: 関西弁キャラ, 30: 赤ちゃんキャラ、指定なし: デフォルトキャラ
-DOCOMO_API_CHARACTER = os.environ.get('DOCOMO_API_CHARACTER')
-
-def get_nickname(lineId):
-    '''LINE情報を取得'''
-    name = 'あなた'
-
-    if lineId:
-        print(LINE_API_PROFILE + '/' + lineId)
-        r = requests.get(LINE_API_PROFILE + '/' + lineId, headers=LINE_HEADERS)
-        if r.status_code == 200:
-            body = r.json()
-            print(body)
-            name = body["displayName"]
-        else:
-            print(r)
-
-    return name
-
-def set_context(lineId, context, mode):
-    '''ユーザごとのコンテキストをredis等に保存'''
-    # ラインIDをキーとしてコンテキスト、モードの保存
-    r = redis.from_url(REDIS_URL)
-    # expire: 180s
-    r.setex(lineId, json.dumps({'context': context, 'mode': mode}), 180)
-
-def get_context(lineId):
-    '''ユーザごとのコンテキストをredis等から取得'''
-    context = ''
-    mode = 'dialog'
-
-    r = redis.from_url(REDIS_URL)
-    val = r.get(lineId)
-    print(val)
-    if val:
-        data = json.loads(val.decode('utf-8'))
-        context = data['context']
-        mode = data['mode']
-
-    return [context, mode]
-
-def __get_dialogue_docomo(text, lineId):
-    response_utt = ''
-    context, mode = get_context(lineId)
-
-    params = {
-        "utt": text,
-        "context": context,
-        "nickname": get_nickname(lineId),
-        "mode": mode,
-        "t": DOCOMO_API_CHARACTER
-    }
-    r = requests.post(
-        DOCOMO_API_DIALOGUE + "?APIKEY=" + DOCOMO_API_KEY,
-        data=json.dumps(params),
-        headers=DOCOMO_HEADERS
-    )
-    if r.status_code == 200:
-        body = r.json()
-        print(body)
-        set_context(lineId, body["context"], body["mode"])
-        response_utt = body["utt"]
-    else:
-        print(r)
-    return response_utt
-
-# def __get_dialogue_dl(text, lineId):
-#     '''内部ディープラーニングを使用してメッセージを生成する'''
-#     return ''
-
-def get_dialogue(text, lineId):
-    '''入力されたテキストに対するレスポンスを生成する'''
-    response_msg = ''
-
-    if MODE == 'docomo':
-        response_msg = __get_dialogue_docomo(text, lineId)
-    else:
-        response_msg = __get_dialogue_dl(text, lineId)
-
-    return response_msg
-
-
-def send_reply(body):
-    for event in body['events']:
-        responses = []
-
-        if event['type'] == 'message':
-            text = ''
-            if event['message']['type'] == 'text':
-                lineId = ''
-                if event['source']['type'] == 'user':
-                    lineId = event['source']['userId']
-                text = get_dialogue(event['message']['text'], lineId)
-            if not text:
-                text = '(´・ω・`)'
-            responses.append({'type': 'text', 'text': text})
-
-        # 返信する
-        reply = {
-            'replyToken': event['replyToken'],
-            'messages': responses
-        }
-        requests.post(LINE_API_REPLY, data=json.dumps(reply), headers=LINE_HEADERS)
+import json
 
 app = Flask(__name__)
 
-@app.route("/webhook", methods=['POST'])
+docomoApiKey = os.environ['DOCOMO_API_KEY']
+REDIS_URL = os.environ['REDIS_URL']
+RedisKey = "context_key"
+
+REPRY_ENDPOINT = 'https://api.line.me/v2/bot/message/reply'
+docomo_endpoint = 'https://api.apigw.smt.docomo.ne.jp/dialogue/v1/dialogue?APIKEY=REGISTER_KEY'
+docomo_url = docomo_endpoint.replace('REGISTER_KEY', docomoApiKey)
+
+FAIRY_STRING = '妖精'
+schedule = "予定"
+register = "登録"
+tano = "たの"
+
+LINE_HEADERS = {
+    'Content-type': 'application/json; charset=UTF-8',
+    'X-Line-ChannelID': os.environ['ChannelID'],
+    'X-Line-ChannelSecret': os.environ['LINE_CHANNEL_SECRET'],
+    'X-Line-Trusted-User-With-ACL': os.environ['MID']
+}
+
+# データベースの指定
+DATABASE_INDEX = 1  # 0じゃなくあえて1
+# コネクションプールから１つ取得
+pool = redis.ConnectionPool.from_url(REDIS_URL, db=DATABASE_INDEX)
+# コネクションを利用
+redisConnection = redis.StrictRedis(connection_pool=pool)
+
+
+def generate_text(text):
+    content = ""
+    if FAIRY_STRING in text:
+        if schedule in text and register in text:
+            # 予定を登録
+            content = "登録フォーム\n https://goo.gl/forms/fjoodUy89O0BFFqv1"
+        else:
+            content = "呼んだ？"
+    elif tano in text:
+        content = "かわいい"
+    else:
+        payload = {'utt': text, 'context': redisConnection.get(RedisKey)}
+        headers = {'Content-type': 'application/json'}
+
+        r = requests.post(docomo_url, data=json.dumps(payload), headers=headers)
+        data = r.json()
+        content = data['utt']
+        context = data['context']
+        redisConnection.set(RedisKey, context)
+    return content
+
+
+def postMessage(replyToken, text):
+    textResponse = [{'type': 'text', 'text': text}]
+    reply = {'replyToken': replyToken, 'messages': textResponse}
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + LINE_HEADERS['X-Line-Trusted-User-With-ACL']
+    }
+    r = requests.post(REPRY_ENDPOINT, data=json.dumps(reply), headers=headers)
+    print(r.text)
+
+
+@app.route('/webhook', methods=['POST'])
 def webhook():
-     print(request.json)
-     send_reply(request.json)
-     return '', 200, {}
+    events = request.json["events"]
+    for event in events:
+        replyToken = event["replyToken"]
+
+        if event['message']['type'] == 'text':
+            text = event['message']['text']
+            content = generate_text(text)
+            postMessage(replyToken, content)
+
+    return ''
+
 
 @app.route('/')
 def hello_world():
-    core_num = mp.cpu_count()
-    config = tf.ConfigProto(
-        inter_op_parallelism_threads=core_num,
-        intra_op_parallelism_threads=core_num )
-    sess = tf.Session(config=config)
-
-    hello = tf.constant('hello, tensorflow!')
-    return sess.run(hello)
+    return 'Hello World!'
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
